1dff3798e41da521beae4aa41a801b1f364cdccb
Shayan <shayansabahi94@gmail.com>
2020-01-27 17:07:00 +0330

    improve docs

--- verticalcardswipe/src/main/java/com/shynline/verticalcardswipe/VerticalCardAdapter.kt
+++ verticalcardswipe/src/main/java/com/shynline/verticalcardswipe/VerticalCardAdapter.kt
@@ -3,15 +3,18 @@ package com.shynline.verticalcardswipe
 import android.view.View
 import android.view.ViewGroup
 
+/***
+ * VerticalCardSwipe base adapter
+ */
 @Suppress("UNCHECKED_CAST")
 abstract class VerticalCardAdapter<T, VH : BaseViewHolder> {
 
     private val items: MutableList<T> = arrayListOf()
 
-    val count: Int
+    internal val count: Int
         get() = items.size
 
-    fun getItem(position: Int): T {
+    internal fun getItem(position: Int): T {
         return items[position]
     }
 
@@ -19,11 +22,16 @@ abstract class VerticalCardAdapter<T, VH : BaseViewHolder> {
         return items
     }
 
+    /***
+     * This will be called when a card has been expired
+     * the view will handle layout automatically
+     *
+     */
     open fun onViewExpired(holder: VH, position: Int) {
         holder.expired = true
     }
 
-    fun removeFirstItem(): Boolean {
+    internal fun removeFirstItem(): Boolean {
         return if (count > 0) {
             items.removeAt(0)
             true
@@ -32,12 +40,25 @@ abstract class VerticalCardAdapter<T, VH : BaseViewHolder> {
         }
     }
 
+    /***
+     * inflate your card view here
+     */
     abstract fun createItemView(parent: ViewGroup): View
 
+    /***
+     * Create your view holder here
+     */
     abstract fun createViewHolder(itemView: View): VH
 
+    /***
+     * This will be called when a card updates
+     *
+     */
     abstract fun onUpdateViewHolder(holder: VH, position: Int)
 
+    /***
+     * This will be called when a card binds
+     */
     abstract fun onBindViewHolder(holder: VH, position: Int)
 
     internal fun getView(position: Int, convert_view: View?, parent: ViewGroup): View {
