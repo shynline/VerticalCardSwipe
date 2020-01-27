10a74f1da7829a60750a943351580ac729855893
Shayan <shayansabahi94@gmail.com>
2020-01-27 15:50:09 +0330

    add comment to ads.kt

--- verticalcardswipe/src/main/java/com/shynline/verticalcardswipe/Ads.kt
+++ verticalcardswipe/src/main/java/com/shynline/verticalcardswipe/Ads.kt
@@ -1,5 +1,9 @@
 package com.shynline.verticalcardswipe
 
 interface Ads {
+    /***
+     * With this method determine if the current
+     * card is an Ad
+     */
     fun isCurrentItemAd(): Boolean
 }
